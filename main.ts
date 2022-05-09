5da391bc2bc790c8a0fce9997274f2a0999afd47
monkmakes <46439220+monkmakes@users.noreply.github.com>
2022-05-09 11:03:33 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     show_wetness = false
     basic.showNumber(PlantMonitor.readTemp())
+    basic.pause(1000)
+    show_wetness = true
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    show_wetness = false
+    basic.showNumber(PlantMonitor.readHumidity())
     basic.pause(1000)
     show_wetness = true
 })
@@ -8,10 +16,11 @@ let show_wetness = false
 PlantMonitor.startMon()
 show_wetness = true
 basic.forever(function () {
-    if (show_wetness) {
+    if (show_wetness == true) {
         led.plotBarGraph(
         PlantMonitor.readWetness(),
         100
         )
+        basic.pause(200)
     }
 })
